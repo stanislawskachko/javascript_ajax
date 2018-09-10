574db29451d06b705892ea8e5587f1f28a7bfb09
Stanislav Skachko <stanislawskachko@gmail.com>
2018-09-10 15:23:29 +0000

    1

---
+++ js/index.js
@@ -0,0 +1,38 @@
+document.querySelector('.time-loaded').innerText 
+ = (new Date()).toLocaleTimeString(); 
+
+
+document.querySelector('.html .get').addEventListener('click', getHtmlData);
+function getHtmlData() {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.querySelector('.html .container').innerHTML 
+                = xhr.responseText;
+        }
+    }
+    xhr.open('GET', 'data.html?r='+Math.random(), true);
+    xhr.send();
+}
+
+document.querySelector('.html .fetch').addEventListener('click', fetchHtmlData);
+function fetchHtmlData() {
+    fetch('data.html')
+        .
+}
+
+document.querySelector('.json .get').addEventListener('click', getJsonData);
+function getJsonData() {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const clientData = JSON.parse(xhr.responseText);
+            document.querySelector('.json .container .name')
+                .innerText = clientData.name;
+            document.querySelector('.json .container .balance')
+                .innerText = clientData.balance;
+        }
+    }
+    xhr.open('GET', 'data.json?r='+Math.random(), true);
+    xhr.send();
+}
